41db9482089adcee6494fa96d7f406dc4b62b5f9
Maksim <agarkov05@inbox.ru>
2022-10-08 10:57:29 +0400

    15 задача

--- Program.cs
+++ Program.cs
@@ -15,33 +15,27 @@
 
 
 
-int a = int.Parse(Console.ReadLine()!);
-int count = 0;
-int te = a;
+// int a = int.Parse(Console.ReadLine()!);
+// int count = 0;
+// int te = a;
 
-while (te>0){
-    count++;
-    te/=10;
-}
-
-
-if (count<3){
-    Console.WriteLine("третьей цифры нема");
-}else {
-
-    for (int i=1; i<=count-3; i++){
-        a/=10;
-    }
-    
-    Console.WriteLine(a%10);
-
-}
+// while (te>0){
+//     count++;
+//     te/=10;
+// }
 
 
+// if (count<3){
+//     Console.WriteLine("третьей цифры нема");
+// }else {
 
+//     for (int i=1; i<=count-3; i++){
+//         a/=10;
+//     }
 
+//     Console.WriteLine(a%10);
 
-
+// }
 
 
 /////////////////////////////////////////////////////////////
@@ -50,6 +44,14 @@ if (count<3){
 // задача 15
 
 
+
+int a = int.Parse(Console.ReadLine()!);
+
+if (a<1 || a>7){
+    Console.WriteLine("введите число от 1 до 7");
+} else if (a>=1 && a<=5) {
+    Console.WriteLine("неть");
+} else Console.WriteLine("да");
 
 
 
