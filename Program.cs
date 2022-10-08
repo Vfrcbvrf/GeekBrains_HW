39f31c68deffeeb3173470439d156af65ea2282a
Maksim <agarkov05@inbox.ru>
2022-10-08 10:52:51 +0400

    13 задача

--- Program.cs
+++ Program.cs
@@ -2,12 +2,8 @@
 
 //  задача 10
 
-int a = int.Parse(Console.ReadLine()!);
-
-
-
-
-Console.WriteLine((a/10)%10);
+// int a = int.Parse(Console.ReadLine()!);
+// Console.WriteLine((a/10)%10);
 
 
 
@@ -16,6 +12,34 @@ Console.WriteLine((a/10)%10);
 
 
 // задача 13
+
+
+
+int a = int.Parse(Console.ReadLine()!);
+int count = 0;
+int te = a;
+
+while (te>0){
+    count++;
+    te/=10;
+}
+
+
+if (count<3){
+    Console.WriteLine("третьей цифры нема");
+}else {
+
+    for (int i=1; i<=count-3; i++){
+        a/=10;
+    }
+    
+    Console.WriteLine(a%10);
+
+}
+
+
+
+
 
 
 
