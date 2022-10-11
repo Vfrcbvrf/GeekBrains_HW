735589e6dd740a9fc171102733b66926fb390935
Maksim <agarkov05@inbox.ru>
2022-10-11 19:54:40 +0400

    21 задача

--- Program.cs
+++ Program.cs
@@ -2,16 +2,16 @@
 
 //  задача 19
 
-string a = Console.ReadLine()!;
-if (a.Length!=5) Console.WriteLine("введите 5-значное число");
-else {
+// string a = Console.ReadLine()!;
+// if (a.Length!=5) Console.WriteLine("введите 5-значное число");
+// else {
 
-    if (a[0]==a[4] && a[1]==a[3]) Console.WriteLine("да");
-    else Console.WriteLine("неа");
+//     if (a[0]==a[4] && a[1]==a[3]) Console.WriteLine("да");
+//     else Console.WriteLine("неа");
 
 
 
-}
+// }
 
 
 
@@ -22,6 +22,23 @@ else {
 
 
 // задача 21
+
+
+Console.WriteLine("введите координаты точки A: ");
+int a1 = int.Parse(Console.ReadLine()!);
+int a2 = int.Parse(Console.ReadLine()!);
+int a3 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите координаты точки B: ");
+int b1 = int.Parse(Console.ReadLine()!);
+int b2 = int.Parse(Console.ReadLine()!);
+int b3 = int.Parse(Console.ReadLine()!);
+
+double ans = Math.Sqrt( Math.Pow((a1-b1),2) + Math.Pow((a2-b2),2) + Math.Pow((a3-b3),2)   );
+
+
+Console.WriteLine(Math.Round(ans,2));
+
+
 
 
 
