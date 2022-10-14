c6032ad29b27a05ffbf05c37874f6b58e7abfc38
Maksim <agarkov05@inbox.ru>
2022-10-14 12:27:29 +0400

    27 задача

--- Program.cs
+++ Program.cs
@@ -1,31 +1,28 @@
 ﻿//  задача 25
 
-// string a = Console.ReadLine()!;
-// if (a.Length!=5) Console.WriteLine("введите 5-значное число");
-// else {
 
-//     if (a[0]==a[4] && a[1]==a[3]) Console.WriteLine("да");
-//     else Console.WriteLine("неа");
 
+// int st(int x, int y){
+
+//     int per = x;
+
+//     if (y!=0){
+    
+//         for (int i=1; i<y; i++){
+
+//             per*=x;
+//         }
+//         return per;
+
+//     } else return 1;
 
 
 // }
 
-int[] st(int x, int y){
 
-    for (int i=1; i<=y; i++){
-
-        x*=x;
-    }
-    return x;
-
-
-}
-
-
-int a = int.Parse(Console.ReadLine()!);
-int b = int.Parse(Console.ReadLine()!);
-Console.WriteLine(st(a, b));
+// int a = int.Parse(Console.ReadLine()!);
+// int b = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(st(a, b));
 
 
 
@@ -33,6 +30,26 @@ Console.WriteLine(st(a, b));
 
 // задача 27
 
+
+int numb(int x){
+
+    int sum = 0;
+    while (x>0){
+
+        sum+= x%10;
+        x/=10;
+
+
+
+    }
+    return sum;
+
+
+}
+
+
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(numb(a));
 
 
 
