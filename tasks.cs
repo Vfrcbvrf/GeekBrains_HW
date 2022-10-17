40e398ad879488e3c0f322065bbeab96a4d55866
Maksim <agarkov05@inbox.ru>
2022-10-17 20:51:45 +0400

    34 задача

--- tasks.cs
+++ tasks.cs
@@ -1,83 +1,89 @@
-﻿//  задача 25
+﻿//  задача 34
 
 
+int[] addarr(int x){
 
-// int st(int x, int y){
-
-//     int per = x;
-
-//     if (y!=0){
-    
-//         for (int i=1; i<y; i++){
-
-//             per*=x;
-//         }
-//         return per;
-
-//     } else return 1;
-
-
-// }
-
-
-// int a = int.Parse(Console.ReadLine()!);
-// int b = int.Parse(Console.ReadLine()!);
-// Console.WriteLine(st(a, b));
-
-
-
-/////////////////////////////////////////////////////////////
-
-// задача 27
-
-
-// int numb(int x){
-
-//     int sum = 0;
-//     while (x>0){
-
-//         sum+= x%10;
-//         x/=10;
-
-
-
-//     }
-//     return sum;
-
-
-// }
-
-
-// int a = int.Parse(Console.ReadLine()!);
-// Console.WriteLine(numb(a));
-
-
-
-
-/////////////////////////////////////////////////////////////
-
-// задача 29
-
-
-
-int[] arr(int x){
-
-    int[] ex = new int[x];
+    int[] ar = new int[x];
 
     for (int i=0; i<x; i++){
 
-        ex[i] = new Random().Next(1000);
+        ar[i] = new Random().Next(100, 999);
+
 
     }
 
-    return ex;
+    return ar;
+
+}
+
+int size(int[] t){
+
+    int count = 0;
+
+    for (int i=0; i<t.Length; i++){
+
+        if (t[i] % 2 == 0) count++;
+
+    
+
+    }
+
+    return count;
+
+
 
 
 }
 
-int[] a = arr(8);
+Console.WriteLine("Введите размер массива");
+int a = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine($"[{String.Join(", ", a) }]");
+
+int[] b = addarr(a);
+
+Console.WriteLine($"[{String.Join(", ", b)}]");
+
+Console.WriteLine(size(b) + " четный(ых) элемент(а)");
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////
+
+// задача 36
+
+
+
+
+
+/////////////////////////////////////////////////////////////
+
+// задача 38
+
+
+
+// int[] arr(int x){
+
+//     int[] ex = new int[x];
+
+//     for (int i=0; i<x; i++){
+
+//         ex[i] = new Random().Next(1000);
+
+//     }
+
+//     return ex;
+
+
+// }
+
+// int[] a = arr(8);
+
+// Console.WriteLine($"[{String.Join(", ", a) }]");
 
 
 
