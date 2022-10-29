c922daefbe276f7c099157e4a72a1ab8dc0710f1
Maksim <agarkov05@inbox.ru>
2022-10-29 19:28:31 +0400

    fe

--- tasks.cs
+++ tasks.cs
@@ -1,168 +1,119 @@
-﻿//  задача 47
+﻿//  задача 60
+
 
 /*
 
+int[,,] getar(int[,,] x){
 
-float[,] get(float[,] x){
+    int m = 0;
+    int l = 9;
+    for (int i=0; i< x.GetLength(0); i++){
+        for (int j=0; j< x.GetLength(1); j++){
+            for (int k=0; k < x.GetLength(2); k++){
+                
+                x[i,j,k] = new Random().Next(m, l);
 
-    for (int i = 0; i<x.GetLength(0); i++){
-        for (int j=0; j<x.GetLength(1); j++){
-            
-            float b = new Random().Next(-100, 100);
-            x[i,j] = b/10;
+                m+=10;
+                l+=10;
+
+
+            }
         }
-
-
     }
 
     return x;
+
+
 }
 
-void print(float[,] ara){
+void print(int[,,] a){
 
-    for (int i=0; i<ara.GetLength(0); i++){
-        for (int j=0; j<ara.GetLength(1); j++){
+    for (int i=0; i< a.GetLength(0); i++){
+        for (int j=0; j< a.GetLength(1); j++){
+            for (int k=0; k< a.GetLength(2); k++){
 
-            Console.Write(ara[i,j] + "  ");
+                Console.Write(a[i,j,k]+ "(" + i + ", " + j + ", " + k + ")  ");
 
+
+
+            }
+            Console.WriteLine();
         }
-        Console.WriteLine();
     }
 
 
+
 }
 
-Console.WriteLine("m= ");
-int m = int.Parse(Console.ReadLine()!);
-Console.WriteLine("n= ");
-int n = int.Parse(Console.ReadLine()!);
 
-float[,] a = new float[m,n];
+int[,,] n = new int[2,2,2];
 
-print(get(a));
+
+getar(n);
+print(n);
 
 */
 
 
 /////////////////////////////////////////////////////////////
 
-// задача 50
-
-
-/*
-
-
-float[,] get(float[,] x){
-
-    for (int i = 0; i<x.GetLength(0); i++){
-        for (int j=0; j<x.GetLength(1); j++){
-            
-            x[i,j] = new Random().Next(-10, 10);
-        }
-
-
-    }
-
-    return x;
-}
-
-void print(float[,] ara){
-
-    for (int i=0; i<ara.GetLength(0); i++){
-        for (int j=0; j<ara.GetLength(1); j++){
-
-            Console.Write(ara[i,j] + "  ");
-
-        }
-        Console.WriteLine();
-    }
-
-
-}
-
-Console.Write("m= ");
-int m = int.Parse(Console.ReadLine()!);
-Console.Write("n= ");
-int n = int.Parse(Console.ReadLine()!);
-
-float[,] a = new float[m,n];
-
-print(get(a));
-
-
-Console.Write("i= ");
-int fir = int.Parse(Console.ReadLine()!);
-Console.Write("j= ");
-int sec = int.Parse(Console.ReadLine()!);
-if (fir>m-1 || sec>n-1) Console.WriteLine("Dont exist");
-    else Console.WriteLine(a[fir, sec]);
-
-
-*/
+// задача 62
 
 
 
-/////////////////////////////////////////////////////////////
-
-// задача 52
 
 
-int[,] get(int[,] x){
-
-    for (int i = 0; i<x.GetLength(0); i++){
-        for (int j=0; j<x.GetLength(1); j++){
-            
-            x[i,j] = new Random().Next(10);
-        }
-
-
-    }
-
-    return x;
-}
-
-void print(int[,] ara){
-
-    for (int i=0; i<ara.GetLength(0); i++){
-        for (int j=0; j<ara.GetLength(1); j++){
-
-            Console.Write(ara[i,j] + "  ");
-
-        }
-        Console.WriteLine();
-    }
-
-
-}
-
-
-int[,] a = new int[4,4];
-
-print(get(a));
-
-
-float ber = 0, count = 0;;
-
-for (int i=0; i<a.GetLength(0); i++){
-
-    ber = 0; count = 0;
-
-    for (int j=0; j<a.GetLength(1); j++){
-
-        ber += a[j, i];
-        count++;
-
-    }
-    Console.Write(ber/count + " ");
-
-
+int[,] getar(int[,] x){
 
     
+    int ch = 0;
+    int i = 0, j = 0;
+    int pre = x.GetLength(0);
+    for (int a = 0; a < pre*pre; a++){
+
+        int k = 0;
+        while (k++ < pre - 1) x[i, j++] = ch++; 
+        
+        for (k = 0; k < pre - 1; k++) x[i++, j] = ch++;
+        for (k = 0; k < pre - 1; k++) x[i, j--] = ch++;
+        for (k = 0; k < pre - 1; k++) x[i--, j] = ch++;
+
+        i++; j++;
+
+        pre = (pre < 2)?0:(pre - 2);
+
+    }
+
+    return x;
+
 }
 
+void print(int[,] a){
+
+    for (int i=0; i<a.GetLength(0); i++){
+        for (int j=0; j< a.GetLength(1); j++){
+            
+            
+            if (a[i,j] < 10) Console.Write("0" + a[i,j] + "  ");
+            else Console.Write(a[i,j] + "  ");
+        }
+        Console.WriteLine();
+    }
 
 
 
+}
+
+int[,] n = new int[4,4];
+
+getar(n);
+
+
+print(n);
+
+
+
+/////////////////////////////////////////////////////////////
 
 
 
